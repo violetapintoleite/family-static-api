97291e0a9d6d6e4a3404f4d7fdac8c52416e3c37
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-06-25 12:15:05 +0000

    Added GET all family members endpoint

--- src/app.py
+++ src/app.py
@@ -26,17 +26,21 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
 
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+  
 
+    return(jsonify(jackson_family.get_all_members())), 200
+   
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    member = jackson_family.get_member_by_id(member_id)
+    
+    return(jsonify(member.serialize())), 200
+
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
